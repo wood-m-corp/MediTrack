b081bb2301215100ea100b072453e3f118576f3d
wood-m-corp <148374240+wood-m-corp@users.noreply.github.com>
2025-07-13 04:42:35 -0400

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module com.example.meditrack {
+module com.example.code {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.meditrack to javafx.fxml;
-    exports com.example.meditrack;
+    opens com.example.code to javafx.fxml;
+    exports com.example.code;
 }
